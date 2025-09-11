e969f7d42a4f52d8312ed2537e1fa33dbe9d2c0a
luisjgro <L24550278@chihuahua2.tecnm.mx>
2025-09-10 18:39:09 -0600

    Add files via upload

---
+++ EVA2_1_EJEMPLO/build/classes/eva2_1_ejemplo/EVA2_1_EJEMPLO.rs
@@ -0,0 +1,2 @@
+eva2_1_ejemplo.EVA2_1_EJEMPLO
+eva2_1_ejemplo.Ejemplo
---
+++ EVA2_1_EJEMPLO/src/eva2_1_ejemplo/EVA2_1_EJEMPLO.java
@@ -0,0 +1,31 @@
+package eva2_1_ejemplo;
+
+public class EVA2_1_EJEMPLO {
+
+    public static void main(String[] args) {
+        Ejemplo miObj = new Ejemplo();
+        miObj.valor = 100;
+        miObj.sig = new Ejemplo();
+        miObj.sig.valor = 200;
+        miObj.sig.sig = new Ejemplo();
+        miObj.sig.sig.valor = 300;
+        miObj.sig.sig.sig = null;
+        
+        Ejemplo temp = miObj;
+        
+        while(temp != null){
+            System.out.print("[" + temp.valor + "]");
+            temp = temp.sig;
+
+    }
+        
+    }
+    
+}
+
+    class Ejemplo{
+        int valor;
+        Ejemplo sig;
+        
+    }
+    
